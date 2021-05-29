ef58dafb58842436943cbf0939c3fb7fffedcaa2
Goooler <wangzongler@gmail.com>
2021-05-29 15:01:52 +0800

    Remove kotlinOptions and compileOptions

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -39,15 +39,6 @@ android {
             dimension = "premium"
         }
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
